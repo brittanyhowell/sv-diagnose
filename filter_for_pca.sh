2f5fd87cbda6047ab6644491dd90bff726e9ef57
brittanyhowell <brittany.howell1@gmail.com>
2019-05-14 23:20:45 +0100

    A lot of filtering Future brie - don't use this approach - convert to plink format and do everything there the world is LOVELY THERE

--- filter_for_pca.sh
+++ filter_for_pca.sh
@@ -7,19 +7,19 @@ wholeVCF="/lustre/scratch119/humgen/projects/cnv_15x/svtools/debug/pca/BATCHmerg
 
 wkDIR=/lustre/scratch119/humgen/projects/cnv_15x/svtools/debug/pca/filter
 
-cd /lustre/scratch119/humgen/projects/cnv_15x/svtools/debug/pca/filter
+cd ${wkDIR}
 
-## Here I'll use bcftools filter to remove variants based on their AC
-echo "/nfs/team151/software/bcftools_2018/bcftools-1.9/bcftools filter -e 'INFO/AC < 2' -O z -o ${wholeVCF%cn.vcf.gz}AC2.vcf.gz ${wholeVCF} "  > run_exclude.sh 
-bsub -G team151 -o "%J_bcf_exclude.o" -R"span[hosts=1]" -R"select[mem>500] rusage[mem=500]" -M500 "bash run_exclude.sh"
+## Here I'll use bcftools filter to remove variants based on their SU
+echo "/nfs/team151/software/bcftools_2018/bcftools-1.9/bcftools filter -e 'INFO/SU < 10 | INFO/SU > 5000' -O z -o ${wholeVCF%cn.vcf.gz}SU.vcf.gz ${wholeVCF} "  > run_exclude_SU.sh 
+bsub -G team151 -o "%J_bcf_exclude.o" -R"span[hosts=1]" -R"select[mem>500] rusage[mem=500]" -M500 "bash run_exclude_SU.sh"
 
-    # NUM: 
+    # job: 762397
 
 ## Here I'll use bcftools filter to remove variants based on their AF
 echo "/nfs/team151/software/bcftools_2018/bcftools-1.9/bcftools filter -e 'INFO/AF < .05' -O z -o ${wholeVCF%cn.vcf.gz}AF_05.vcf.gz  ${wholeVCF} "  > run_exclude_AF_05.sh 
 
 bsub -G team151 -o "%J_bcf_exclude.o" -R"span[hosts=1]" -R"select[mem>500] rusage[mem=500]" -M500 "bash run_exclude_AF_05.sh"
-# 745350
+# 23295 remaining
 
 ## Here I'll use bcftools filter to remove variants based on their MSQ
 echo "/nfs/team151/software/bcftools_2018/bcftools-1.9/bcftools filter -e 'INFO/MSQ < 100' -O z -o ${wholeVCF%cn.vcf.gz}MSQ_100.vcf.gz  ${wholeVCF} "  > ${wkDIR}/run_exclude_MSQ_100.sh 
@@ -27,3 +27,6 @@ echo "/nfs/team151/software/bcftools_2018/bcftools-1.9/bcftools filter -e 'INFO/
 bsub -G team151 -o "%J_bcf_exclude.o" -R"span[hosts=1]" -R"select[mem>500] rusage[mem=500]" -M500 "bash ${wkDIR}/run_exclude_MSQ_100.sh"
 
 # Vars: 25181 
+
+
+/nfs/team151/software/bcftools_2018/bcftools-1.9/bcftools filter -e 'INFO/SU < 10 | INFO/SU > 5000'  ${wholeVCF} | less -S
